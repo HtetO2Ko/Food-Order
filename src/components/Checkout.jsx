354a54b4e49ffc74921735e480bab6ddc740efa1
HtetO2Ko <hteto2ko.dev@gmail.com>
2026-09-26 02:01:26 +0630

    HTTP Request header to headers

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -25,13 +25,13 @@ export default function Checkout() {
     const fd = new FormData(event.target);
     const customerData = Object.fromEntries(fd.entries());
 
-    fetch("http:localhost:3000/orders", {
+    fetch("http://localhost:3000/orders", {
       method: "POST",
-      header: {
+      headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        orders: {
+        order: {
           items: cartCtx.items,
           customer: customerData,
         },
